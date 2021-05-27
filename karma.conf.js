411b540605f07073b122bc8422a28f2b3ad80f82
JackWithOneEye <sroettger92@gmail.com>
2021-05-27 19:14:22 +0200

    linting & formatting

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/single-page-app-test'),
+      dir: require('path').join(__dirname, './coverage/mm-test-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
